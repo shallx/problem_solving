4726d8ab11053e565acc5b7adc5bae7db08a3288
Rafat Rashid Rahi <rafat.rashid247@gmail.com>
2023-03-01 15:15:13 +0600

    Leetcode 20 done

---
+++ leetcode/20_valid_parentheses.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <stack>
+#include <string.h>
+
+using namespace std;
+
+int main(){
+    string s = "()[]{}";
+
+    int length = s.length();
+
+    stack<char> st;
+
+    for(int i=0;i<length;i++){
+        if(s[i] == '(' || s[i] == '[' || s[i] == '{'){
+            st.push(s[i]);
+        }
+        else if((s[i] == ')' || s[i] == ']' || s[i] == '}') && st.empty()){
+            return false;
+        }
+        else if((s[i] == ')' && st.top() == '(') || (s[i] == '}' && st.top() == '{') || (s[i] == ']' && st.top() == '[')){
+            st.pop();
+        }
+        else {
+            return false;
+        }
+
+    }
+    return st.empty() ? true : false;
+
+}
