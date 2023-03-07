2a93b7302044851adde51563a71cfcb893b6aeeb
shallx <rafat.rashid247@gmail.com>
2023-03-08 03:52:01 +0600

    Solved 26

--- leetcode/26.cpp
+++ leetcode/26.cpp
@@ -8,7 +8,6 @@ public:
     int removeDuplicates(vector<int>& nums) {
         int n = nums.size();
         int diff = nums[n-1] - nums[0] + 1;
-        // printf("%d %d %d\n", n, nums[n-1], nums[0]);
         int matching = nums[0];
         int index = 1;
         for(int i=1; i<n;i++){
@@ -18,17 +17,12 @@ public:
                 index++;
             }
         }
-        for(auto i: nums){
-            cout << i << " ";
-        }
-        cout << endl;
-        return diff;
+        return index;
     }
 };
 
 int main(){
     vector<int> v{0,0,1,1,1,2,2,3,3,4};
-    // vector<int> v{1,1,2};
 
     Solution sol = Solution();
     cout << sol.removeDuplicates(v) << endl;
