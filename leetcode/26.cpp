4df1886ccab0a92f19216c837d1ae64a60bda441
shallx <rafat.rashid247@gmail.com>
2023-03-08 00:48:07 +0600

    Solved 326, solving 26

--- leetcode/26.cpp
+++ leetcode/26.cpp
@@ -1,19 +1,34 @@
 #include <vector>
 #include <iostream>
+#include <stdio.h>
 
 using namespace std;
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
         int n = nums.size();
-        int diff = n - (nums[n-1] - nums[0]) + 1;
-
+        int diff = nums[n-1] - nums[0] + 1;
+        // printf("%d %d %d\n", n, nums[n-1], nums[0]);
+        int matching = nums[0];
+        int index = 1;
+        for(int i=1; i<n;i++){
+            if(nums[i] > matching){
+                matching = nums[i];
+                nums[index] = matching;
+                index++;
+            }
+        }
+        for(auto i: nums){
+            cout << i << " ";
+        }
+        cout << endl;
         return diff;
     }
 };
 
 int main(){
-    vector<int> v{1,2,3,3};
+    vector<int> v{0,0,1,1,1,2,2,3,3,4};
+    // vector<int> v{1,1,2};
 
     Solution sol = Solution();
     cout << sol.removeDuplicates(v) << endl;
--- leetcode/326.cpp
+++ leetcode/326.cpp
@@ -6,25 +6,16 @@ using namespace std;
 
 class Solution {
 public:
-    float log_a_to_base_3(int a)
-    {
-        return log2(a) / log2(3);
-    }
     bool isPowerOfThree(int n) {
         if(n == 0) return false;
-        float res = log_a_to_base_3(n);
-        float flooredRes = floor(res);
-        printf("%f %f\n", res, flooredRes);
-        if(res == flooredRes) {
-            return true;
-        }
-        
-        else return false;
+        if(n == 1) return true;
+        else if(n % 3 != 0) return false;
+        else return isPowerOfThree(n / 3);
     }
 };
 
 int main(){
     Solution sol = Solution();
-    cout << sol.isPowerOfThree(4782968) << endl;
+    cout << sol.isPowerOfThree(25) << endl;
     return 0;
 }
