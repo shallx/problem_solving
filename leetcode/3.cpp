66a5059cd0b08318d7f51b1329cdfd0f5c853b0a
shallx <rafat.rashid247@gmail.com>
2023-03-11 03:35:03 +0600

    Solved leet-3

--- leetcode/3.cpp
+++ leetcode/3.cpp
@@ -1,31 +1,52 @@
 #include <iostream>
 #include <stdio.h>
 #include <string.h>
+#include <list>
+#include <algorithm>
 
 using namespace std;
 
 class Solution {
 public:
+    void showLS(list<char>& ls){
+        printf("List(%d):", ls.size());
+        for(auto i : ls){
+            cout << i;
+        }
+        cout << endl;
+
+    }
+    bool contains(list<char>& ls, char value){
+        list<char>::iterator it = find(ls.begin() , ls.end() , value);
+        return it != ls.end();
+    }
+    
+    // remove until X and add value to end
+    int removeUntilXFromSet(list<char>& ls, char value){
+        while(ls.front() != value){
+            ls.pop_front();
+        }
+        ls.pop_front();
+        ls.push_back(value);
+        return ls.size();
+
+    }
     int lengthOfLongestSubstring(string s) {
-        string checkingSub = "";
-        checkingSub += s[0];
-        char checkingChar = s[0];
+        
+        if( s == "") return 0;
+        list<char> checkingSub;
+        checkingSub.push_back(s[0]);
+        char start = s[0];
+        char end = s[0];
         int mx = 1,count=1, ln = s.size();
-        cout << "CheckingSub: " << checkingSub[0]<< endl;
-        cout << "CheckingChar: " << checkingChar << endl;
         for(int i=1; i<ln;i++){
-            if(checkingChar != s[i] && checkingChar != checkingSub[0]) {
-                cout << "Checking: " << s[i] << endl;
-                checkingSub+=s[i];
-                checkingChar= s[i];
+            if(!contains(checkingSub, s[i])) {
+                checkingSub.push_back(s[i]);
                 count++;
-                checkingSub = max(count,mx);
-            } else {
-                cout << "Else: " << s[i] << endl;
-                checkingSub.clear();
-                checkingSub += s[i];
-                checkingChar = s[i];
-                count = 1;
+                mx = max(count,mx);
+            } 
+            else {
+                count = removeUntilXFromSet(checkingSub, s[i]);
             }
         }
         return mx;
@@ -34,7 +55,7 @@ public:
 };
 
 int main(){
-    string s = "adibasi";
+    string s = "aabaab!bb";
     Solution sol = Solution();
     cout << sol.lengthOfLongestSubstring(s) << endl;
 
