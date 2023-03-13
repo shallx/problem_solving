45201d9649dfd017934fe70cc684571950392cc4
Rafat Rashid Rahi <rafat.rashid247@gmail.com>
2023-03-13 22:32:21 +0600

    Airwrk solved

---
+++ leetcode/array/airwrk.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main(){
+    int arr[] = {1,0,2,3,0,5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    int i = 0;
+    int j = n-1;
+    while(1){
+        if()
+    }
+
+    return 0;
+}
