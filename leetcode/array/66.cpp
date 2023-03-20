4f9067956f9a0a6e0373f27bf1e54527a1bbd962
Rafat Rashid Rahi <rafat.rashid247@gmail.com>
2023-03-20 18:00:36 +0600

    workign on 66

---
+++ leetcode/array/66.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> plusOne(vector<int> &digits)
+    {
+    }
+};
+
+int main()
+{
+    vector<int> v{1, 2, 3};
+    Solution sol = Solution();
+    sol.plusOne(v);
+
+    return 0;
+}
