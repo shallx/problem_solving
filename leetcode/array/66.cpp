27c7b102593e39565730be1f5331baf4d0e18af2
Rafat Rashid Rahi <rafat.rashid247@gmail.com>
2023-03-21 17:57:53 +0600

    Solved 66

--- leetcode/array/66.cpp
+++ leetcode/array/66.cpp
@@ -7,16 +7,34 @@ using namespace std;
 class Solution
 {
 public:
-    vector<int> plusOne(vector<int> &digits)
+    vector<int> plusOne(vector<int> &d)
     {
+        
+        int carry = 1;
+        for(int i = d.size()-1; i >=0; i--){
+            int k = d[i] + carry;
+            carry =  0;
+            if(k > 9){
+                d[i] = k % 10;
+                carry = k/10;
+            } else d[i] = k;
+        }
+        if(carry > 0){
+            d.insert(d.begin(), carry);
+        }
+        return d;
     }
 };
 
 int main()
 {
-    vector<int> v{1, 2, 3};
+    vector<int> v{9};
     Solution sol = Solution();
     sol.plusOne(v);
+    for(auto it : v){
+        cout << it << " ";
+    }
+    cout << endl;
 
     return 0;
 }
